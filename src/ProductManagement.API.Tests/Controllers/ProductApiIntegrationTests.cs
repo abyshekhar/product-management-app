570cce3081f64a28248dfb7f10f467305d23bc3c
Sudhanshu Shekhar <abyshekhar@gmail.com>
2025-09-01 16:44:27 +0530

    Restructuring the tests

---
+++ src/ProductManagement.API.Tests/Controllers/ProductApiIntegrationTests.cs
@@ -0,0 +1,25 @@
+using System.Net.Http.Json;
+using Xunit;
+namespace ProductManagement.API.Tests.Controllers
+{
+    public class ProductApiIntegrationTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ProductApiIntegrationTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task CreateProduct_ReturnsCreatedProduct()
+        {
+
+            var product = new { Name = "Integration Test Product", Description = "Integration Test Product", Price = 99, CategoryId = 1 };
+
+            var response = await _client.PostAsJsonAsync("/api/products", product);
+
+            Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+        }
+    }
+}
